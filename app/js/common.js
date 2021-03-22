78433b61cbaf8a5751e46702de0f360341884315
nigilen <nigilen@gmail.com>
2021-03-22 22:25:13 +0200

    ...

--- app/js/common.js
+++ app/js/common.js
@@ -10,25 +10,30 @@ burger.addEventListener('click', openMenu);
 
 let menuList = document.querySelectorAll('.menu-list');
 let toggleItems = document.querySelectorAll('.toggles-item');
-let toggleDescItems = document.querySelectorAll('.toggle-item--desctop');
-
-let toggleMenu = function (evt) {
-  
-}
+let toggleDescItems = document.querySelectorAll('.toggle-item--desktop');
 
 
-for (let i = 0; i < toggleItems.length; i++) {
-  toggleItems[i].addEventListener('click', function(evt) {
-    let target = evt.target;
 
-    menuList[i].classList.add('menu-list--active'); 
 
-    if (target.classList.contains('menu-list--active') === false) {
-      for (let j = 0; j < menuList.length; j++) {
-        menuList[j].classList.remove('menu-list--active'); 
-      }
+
+for (let i = 0; i < menuList.length; i++) {
+  toggleItems[i].addEventListener('click', function (evt) {
+    for (let j = 0; j < menuList.length; j++) {
+      menuList[j].classList.remove('menu-list--active');
     }
-    
+    menuList[i].classList.add('menu-list--active');
   });
 };
+
+for (let i = 0; i < menuList.length; i++) {
+  toggleDescItems[i].addEventListener('click', function (evt) {
+    for (let j = 0; j < menuList.length; j++) {
+      menuList[j].classList.remove('menu-list--active');
+    }  
+    menuList[i].classList.add('menu-list--active');
+  });
+};
+
+
+
 
