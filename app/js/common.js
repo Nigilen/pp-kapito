af830826d0cb3323c3c022a007d0ad9e1c4ef261
absolen <nigilen@gmail.com>
2021-03-22 16:39:33 +0200

    menu...

--- app/js/common.js
+++ app/js/common.js
@@ -6,3 +6,29 @@ let openMenu = function() {
 }
 
 burger.addEventListener('click', openMenu);
+
+
+let menuList = document.querySelectorAll('.menu-list');
+let toggleItems = document.querySelectorAll('.toggles-item');
+let toggleDescItems = document.querySelectorAll('.toggle-item--desctop');
+
+let toggleMenu = function (evt) {
+  
+}
+
+
+for (let i = 0; i < toggleItems.length; i++) {
+  toggleItems[i].addEventListener('click', function(evt) {
+    let target = evt.target;
+
+    menuList[i].classList.add('menu-list--active'); 
+
+    if (target.classList.contains('menu-list--active') === false) {
+      for (let j = 0; j < menuList.length; j++) {
+        menuList[j].classList.remove('menu-list--active'); 
+      }
+    }
+    
+  });
+};
+
