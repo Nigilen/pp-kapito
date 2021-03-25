380a7a7ef81828a19e0f1d5f42cbfb0130f5f27d
absolen <nigilen@gmail.com>
2021-03-25 16:51:57 +0200

    fix header

--- app/js/menu.js
+++ app/js/menu.js
@@ -2,8 +2,8 @@ let header = document.querySelector('.header');
 
 window.addEventListener('scroll', function() {
   if (window.pageYOffset > 400) {
-    header.classList.add('header-fixed');
+    header.classList.add('header--fixed');
   } else {
-    header.classList.remove('header-fixed');
+    header.classList.remove('header--fixed');
   }
 })
